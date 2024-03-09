9bd11c4754ca0b8d96775b91398ada92a7b9956b
Mohammad Hamza <mhdkh444@gmail.com>
2024-03-09 18:30:56 +0530

    navbar files

--- Navbar-with-React/src/App.jsx
+++ Navbar-with-React/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import "./App.css"
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import { useState } from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [isActive, setIsActive] = useState(false);
+
+  const navActive = ()=>{
+    return setIsActive(!isActive);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav>
+        <h1>LOGO</h1>
+        <ul className={isActive ? "active": ""}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">User</a></li>
+          <li><a href="#">Github</a></li>
+        </ul>
+        {
+          isActive? <IoClose onClick={navActive}/> : <FaBars onClick={navActive} className='bar-icon'/>
+        }
+      </nav>
+      <div className='bg-image'>
+        <img src="https://images.unsplash.com/photo-1559305289-4c31700ba9cb?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="bg image" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
