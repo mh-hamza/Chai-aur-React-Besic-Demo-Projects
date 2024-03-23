f6038d20a7a15624dc602e223b75ac624b1d610c
Mohammad Hamza <mhdkh444@gmail.com>
2024-03-23 11:26:35 +0530

    add

--- React-map-methoud/src/App.jsx
+++ React-map-methoud/src/App.jsx
@@ -5,13 +5,13 @@ const cardData = [
     src: 'https://images.unsplash.com/photo-1560790671-b76ca4de55ef?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Flower',
     description: "Flower is a simple tool to generate and manipulate files",
-    id : 1
+    id: 1
   },
   {
     src: 'https://images.unsplash.com/photo-1491918434088-6a06cd4254ef?q=80&w=1280&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Flower',
     description: "Flower is a simple tool to generate and manipulate files",
-    id : 2
+    id: 2
   },
   {
     src: 'https://images.unsplash.com/photo-1560790671-b76ca4de55ef?q=80&w=1034&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -29,25 +29,26 @@ const cardData = [
 ]
 function App() {
   let foodItem = ["Fruit", "vegetables", "Meat", "Fish", "Bread", "Potato"];
+
   return (
     <>
       <div className="container">
         <h1>Food Items</h1>
         <ul>
           {foodItem.map((items) => {
-            return <li key={items}>{items}</li> ;
+            return <li key={items}>{items}</li>;
           })}
         </ul>
         <h1>Card Items</h1>
         <div className='card-container'>
           {
-            cardData.map((items)=>{
+            cardData.map((items) => {
               return (
-                <Card 
-                key={items.id}
-                src={items.src}
-                title={items.title}
-                description={items.description}
+                <Card
+                  key={items.id}
+                  src={items.src}
+                  title={items.title}
+                  description={items.description}
                 />
               )
             })
